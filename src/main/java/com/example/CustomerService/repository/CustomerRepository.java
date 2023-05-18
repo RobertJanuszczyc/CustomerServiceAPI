253fe664bb3e4fff8643b2ade91c5f851455436b
Robert Januszczyc <robertjanuszczyc2@gmail.com>
2023-05-18 21:43:43 +0200

    Third commit

---
+++ src/main/java/com/example/CustomerService/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.CustomerService.repository;
+
+import com.example.CustomerService.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
