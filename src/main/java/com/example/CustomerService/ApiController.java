4daffd0c910636d1ce5fec537fb16877f3b00e98
Robert Januszczyc <robertjanuszczyc2@gmail.com>
2023-05-24 18:48:04 +0200

    Relationship between Event class and Customer class
    Author: Robert Januszczyc
    Date: Wed May 23 2023
    
    Commit is connected with following changes:
    -Relationship between Event class and Customer class has been added

--- src/main/java/com/example/CustomerService/ApiController.java
+++ src/main/java/com/example/CustomerService/ApiController.java
@@ -1,12 +1,10 @@
 package com.example.CustomerService;
 
 import com.example.CustomerService.repository.CustomerRepository;
-import com.example.CustomerService.repository.MenuRepository;
 import com.example.CustomerService.repository.EventRepository;
+import com.example.CustomerService.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,55 +15,55 @@ public class ApiController {
     private final MenuRepository menuRepository;
     private final EventRepository eventRepository;
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
-    }
-
-    @GetMapping("/customers")
-    public List<Customer> getCustomers() {
-        return customerRepository.findAll();
-    }
-
-    @PostMapping("/customers")
-    public Customer createCustomer(@RequestBody Customer customer) {
-        return customerRepository.save(customer);
-    }
-
-    @DeleteMapping("/customers")
-    public void deleteCustomer(@RequestParam Long id) {
-        customerRepository.deleteById(id);
-    }
-
-    @GetMapping("/menu")
-    public List<Menu> getMenu() {
-        return menuRepository.findAll();
-    }
-
-    @PostMapping("/menu")
-    public Menu createMenu(@RequestBody Menu menu) {
-        return menuRepository.save(menu);
-    }
-
-    @DeleteMapping("/menu")
-    public void deleteMenu(@RequestParam int id) {
-        menuRepository.deleteById(id);
-    }
-
-    @GetMapping("/event")
-    public List<Event> getEvent() {
-        return eventRepository.findAll();
-    }
-
-    @PostMapping("/event")
-    public Event createEvent(@RequestBody Event event) {
-        return eventRepository.save(event);
-    }
-
-    @DeleteMapping("/event")
-    public void deleteEvent(@RequestParam long id) {
-        eventRepository.deleteById(id);
-    }
+//    @GetMapping("/hello")
+//    public String sayHello() {
+//        return "Hello World!";
+//    }
+//
+//    @GetMapping("/customers")
+//    public List<Customer> getCustomers() {
+//        return customerRepository.findAll();
+//    }
+//
+//    @PostMapping("/customers")
+//    public Customer createCustomer(@RequestBody Customer customer) {
+//        return customerRepository.save(customer);
+//    }
+//
+//    @DeleteMapping("/customers")
+//    public void deleteCustomer(@RequestParam Long id) {
+//        customerRepository.deleteById(id);
+//    }
+//
+//    @GetMapping("/menu")
+//    public List<Menu> getMenu() {
+//        return menuRepository.findAll();
+//    }
+//
+//    @PostMapping("/menu")
+//    public Menu createMenu(@RequestBody Menu menu) {
+//        return menuRepository.save(menu);
+//    }
+//
+//    @DeleteMapping("/menu")
+//    public void deleteMenu(@RequestParam int id) {
+//        menuRepository.deleteById(id);
+//    }
+//
+//    @GetMapping("/event")
+//    public List<Event> getEvent() {
+//        return eventRepository.findAll();
+//    }
+//
+//    @PostMapping("/event")
+//    public Event createEvent(@RequestBody Event event) {
+//        return eventRepository.save(event);
+//    }
+//
+//    @DeleteMapping("/event")
+//    public void deleteEvent(@RequestParam long id) {
+//        eventRepository.deleteById(id);
+//    }
 
 
 
--- src/main/java/com/example/CustomerService/Event.java
+++ src/main/java/com/example/CustomerService/Event.java
@@ -3,16 +3,26 @@ package com.example.CustomerService;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Entity
 @Data
 public class Event {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
-
     private Long id;
+
     private String eventName;
     private int qtyPeople;
+
+    @OneToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+            name = "event_id",
+            referencedColumnName = "id"
+    )
+    private List<Customer> customers;
 
 }
