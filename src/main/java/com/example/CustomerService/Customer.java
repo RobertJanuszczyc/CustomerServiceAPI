b4db89933aa8b9a266593eb21f048c47ce0dc70e
Robert Januszczyc <robertjanuszczyc2@gmail.com>
2023-05-27 21:04:59 +0200

    Validation in the Customer class
    Author: Robert Januszczyc
    Date: Sat May 27 2023
    
    Commit is related to the following changes:
    -Validation for firstName, lastName, email, phoneNumber fields has been added

--- src/main/java/com/example/CustomerService/Customer.java
+++ src/main/java/com/example/CustomerService/Customer.java
@@ -1,6 +1,9 @@
 package com.example.CustomerService;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
@@ -19,13 +22,17 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    @NotNull(message = "first name cannot be a null")
     private String firstName;
+    @NotNull(message = "last name cannot be a null")
     private String lastName;
+    @Pattern(regexp = "^\\d{10}$" , message = "phone number must have exactly nine digits")
     private String phoneNumber;
     @Column(
             nullable = false
     )
+
+    @Email(message = "invalid email address")
     private String email;
 
     @ManyToOne(cascade = CascadeType.ALL)
