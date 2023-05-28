baeae14da1440efc19f8fd81cfa551c5909a3648
Robert Januszczyc <robertjanuszczyc2@gmail.com>
2023-05-28 10:54:05 +0200

    New class ApplicationExceptionHandler
    Author: Robert Januszczyc
    Date: Sun May 28 2023

---
+++ src/main/java/com/example/CustomerService/ApplicationExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.CustomerService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error -> errorMap.put(error.getField(), error.getDefaultMessage()));
+
+        return errorMap;
+
+    }
+}
